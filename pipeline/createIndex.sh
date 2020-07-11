1c68182265339bcc54b945f6cc003dbdfc35ab8f
nikhil pandit <npandit@saba.com>
2020-07-11 10:25:59 +0000

    added section to create build no

--- pipeline/createIndex.sh
+++ pipeline/createIndex.sh
@@ -8,6 +8,7 @@ cat > index.html <<EOF
 <h1 style="color:blue;text-align:center;font-size:80px;">NIKHIL PANDIT</h1>
 <p style="color:red;text-align:center;font-size:40px;">DEVOPS ENG.</p>
 <p style="color:green;text-align:center;font-size:30px;">DATE: <MY-DATE>.</p>
+<p style="color:red;text-align:center;font-size:40px;">BUILD NO : ${BUILD_NUMBER}</p>
 </body>
 </html>
 EOF
